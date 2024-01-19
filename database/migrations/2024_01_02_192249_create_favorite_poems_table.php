dca3da090066eb269e990686ee7b7f849ba63783
ale :) <alelaalien@gmail.com>
2024-01-19 19:33:41 -0300

    thought_tag

--- database/migrations/2024_01_02_192249_create_favorite_poems_table.php
+++ database/migrations/2024_01_02_192249_create_favorite_poems_table.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -6,27 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('favorite_poems', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('poem_id');
-            $table->timestamps();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('poem_id')->references('id')->on('poems')->onDelete('cascade');
-            $table->unique(['user_id', 'poem_id']);
-        });
-    }
+ 
+    // public function up(): void
+    // {
+    //     Schema::create('favorite_poems', function (Blueprint $table) {
+    //         $table->id();
+    //         $table->unsignedBigInteger('user_id');
+    //         $table->unsignedBigInteger('poem_id');
+    //         $table->timestamps();
+    //         $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+    //         $table->foreign('poem_id')->references('id')->on('poems')->onDelete('cascade');
+    //         $table->unique(['user_id', 'poem_id']);
+    //     });
+    // }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('favorite_poems');
-    }
-};
+   
+    // public function down(): void
+    // {
+    //     Schema::dropIfExists('favorite_poems');
+    // }
+};  
